8a02a12cfb3b885e0346e9ca24b0c2b9a2cdfc6e
Otavio Landim <otavio.landim@gmail.com>
2024-07-23 12:01:18 -0300

    fix: exerc1

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,8 +1,37 @@
 describe('basic', () => {
   describe('Central de Atendimento ao Cliente TAT', () => {
-    it('verifica o título da aplicação', () => {
+
+    beforeEach(() => {
       cy.visit('../../src/index.html')
-      // cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    });
+
+    it('verifica o título da aplicação', () => {
+      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+      cy.get('#firstName')
+        .should('be.visible')
+        .type('Joao')
+        .should('have.value', 'Joao')
+      cy.get('#lastName')
+        .should('be.visible')
+        .type('Landim')
+        .should('have.value', 'Landim')
+      cy.get('#email')
+        .should('be.visible')
+        .type('joao@email.com')
+        .should('have.value', 'joao@email.com')
+      cy.get('#open-text-area')
+        .should('be.visible')
+        .type('Otimo curso')
+        .should('have.value', 'Otimo curso')
+      cy.contains('button', 'Enviar')
+        .should('be.visible')
+        .click()
+      cy.get('.success')
+        .should('be.visible')
+        .and('contain', 'Mensagem enviada com sucesso')
     })
   });
  
