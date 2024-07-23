f7b56a854534477e28f8cffb59915ba89b746827
Otavio Landim <otavio.landim@gmail.com>
2024-07-23 14:52:03 -0300

    fix: exerc1 extra 4

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -74,6 +74,34 @@ describe('basic', () => {
         .type('joao@email.com', { delay: 0})
         .should('have.value', '')
     });
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+      cy.get('#firstName')
+        .should('be.visible')
+        .type('Joao')
+        .should('have.value', 'Joao')
+      cy.get('#lastName')
+        .should('be.visible')
+        .type('Landim')
+        .should('have.value', 'Landim')
+      cy.get('#email')
+        .should('be.visible')
+        .type('joao@email.com')
+        .should('have.value', 'joao@email.com')
+      cy.get('#phone-checkbox')
+        .should('be.visible')
+        .check()
+      cy.get('#open-text-area')
+        .should('be.visible')
+        .type('Otimo curso')
+        .should('have.value', 'Otimo curso')
+      cy.contains('button', 'Enviar')
+        .should('be.visible')
+        .click()
+      cy.get('.error')
+        .should('be.visible')
+        .and('contain', 'Valide os campos obrigatórios')
+    });
   });
  
 })
