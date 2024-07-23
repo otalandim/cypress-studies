6973e3b72661540cdc2fceb386191ddbd04c5d77
Otavio Landim <otavio.landim@gmail.com>
2024-07-23 11:10:51 -0300

    test: add structure

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,9 @@
+describe('basic', () => {
+  describe('Central de Atendimento ao Cliente TAT', () => {
+    it('verifica o título da aplicação', () => {
+      cy.visit('../../src/index.html')
+      // cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+  });
+ 
+})
